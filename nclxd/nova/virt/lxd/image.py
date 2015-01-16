64d1606b01d9f7469c4111c7fa3b863e7bb6c04a
Chuck Short <chuck.short@canonical.com>
2015-01-16 10:19:33 -0500

    Add missing files
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

---
+++ nclxd/nova/virt/lxd/image.py
@@ -0,0 +1,76 @@
+import os
+
+from nova import utils
+from nova.i18n import _, _LW
+from nova.openstack.common import log as logging
+
+from . import utils as container_utils
+
+LOG = logging.getLogger(__name__)
+
+def write_image(idmap, image, root_dir):
+    tar = ['tar', '--directory', root_dir,
+           '--anchored', '--numeric-owner', '-xpzf', image]
+    nsexec = (['lxc-usernsexec'] +
+              idmap.usernsexec_margs(with_read="user") +
+              ['--'])
+    args = tuple(nsexec + tar)
+    utils.execute(*args, check_exit_code=[0, 2])
+
+class ContainerImage(object):
+    def __init__(self, **kwargs):
+        super(ContainerImage, self).__init__()
+
+    def create_container(self):
+        pass
+
+    def remove_contianer(self):
+        pass
+
+class ContainerLocal(ContainerImage):
+    def __init__(self, image, instance, root_dir):
+        super(ContainerLocal, self).__init__()
+        self.image = image
+        self.instance = instance
+        self.root_dir = root_dir
+
+        self.idmap = container_utils.LXCUserIdMap()
+
+    def create_container(self):
+        (user, group) = self.idmap.get_user()
+        utils.execute('chown', '%s:%s' % (user, group), self.root_dir,
+                       run_as_root=True)
+        write_image(self.idmap, self.image, self.root_dir)
+
+    def remove_container(self):
+        pass
+
+class ContainerCoW(ContainerImage):
+    def __init__(self, image, instance, root_dir, base_dir):
+        super(ContainerCoW, self).__init__()
+        self.idmap = container_utils.LXCUserIdMap()
+        self.image = image
+        self.instance = instance
+        self.root_dir = root_dir
+        self.base_dir = base_dir
+
+    def create_container(self):
+        image_dir = os.path.join(self.base_dir, self.instance['image_ref'])
+        if not os.path.exists(image_dir):
+            (user, group) = self.idmap.get_user()
+            utils.execute('btrfs', 'subvolume', 'create', image_dir)
+            utils.execute('chown', '%s:%s' % (user, group), image_dir,
+                          run_as_root=True)
+            write_image(self.idmap, self.image, image_dir)
+
+        utils.execute('btrfs', 'subvolume', 'snapshot', image_dir,
+                      self.root_dir, run_as_root=True)
+        size = self.instance['root_gb']
+        if size != 0:
+            utils.execute('btrfs', 'quota', 'enable', self.root_dir,
+                          run_as_root=True)
+            utils.execute('btrfs', 'qgroup', 'limit', '%sG' % size,
+                          self.root_dir, run_as_root=True)
+
+    def remove_container(self):
+        pass
---
+++ nclxd/nova/virt/lxd/utils.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2014 Canonical ltd
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import grp
+import getpass
+import pwd
+import os
+
+
+from oslo.config import cfg
+
+from nova.openstack.common import log as logging
+from nova import context as nova_context
+from nova import objects
+from nova import utils
+
+LOG = logging.getLogger(__name__)
+
+
+CONF = cfg.CONF
+
+
+def write_lxc_usernet(instance, bridge, user=None, count=1):
+    if user is None:
+        user = getpass.getuser()
+    utils.execute('lxc-usernet-manage', 'set', user, bridge, str(count),
+                  run_as_root=True, check_exit_code=[0])
+
+
+class LXCIdMap(object):
+
+    def __init__(self, ustart, unum, gstart, gnum):
+        self.ustart = int(ustart)
+        self.unum = int(unum)
+        self.gstart = int(gstart)
+        self.gnum = int(gnum)
+
+    def usernsexec_margs(self, with_read=None):
+        if with_read:
+            if with_read == "user":
+                with_read = os.getuid()
+            unum = self.unum - 1
+            rflag = ['-m', 'u:%s:%s:1' % (self.ustart + self.unum, with_read)]
+            print(
+                "================ rflag: %s ==================" %
+                (str(rflag)))
+        else:
+            unum = self.unum
+            rflag = []
+
+        return ['-m', 'u:0:%s:%s' % (self.ustart, unum),
+                '-m', 'g:0:%s:%s' % (self.gstart, self.gnum)] + rflag
+
+    def lxc_conf_lines(self):
+        return (('lxc.id_map', 'u 0 %s %s' % (self.ustart, self.unum)),
+                ('lxc.id_map', 'g 0 %s %s' % (self.gstart, self.gnum)))
+
+    def get_user(self):
+        return (self.ustart, self.gstart)
+
+
+class LXCUserIdMap(LXCIdMap):
+
+    def __init__(self, user=None, group=None, subuid_f="/etc/subuid",
+                 subgid_f="/etc/subgid"):
+        if user is None:
+            user = pwd.getpwuid(os.getuid())[0]
+        if group is None:
+            group = grp.getgrgid(os.getgid()).gr_name
+
+        def parse_sfile(fname, name):
+            line = None
+            with open(fname, "r") as fp:
+                for cline in fp:
+                    if cline.startswith(name + ":"):
+                        line = cline
+                        break
+            if line is None:
+                raise ValueError("%s not found in %s" % (name, fname))
+            toks = line.split(":")
+            return (toks[1], toks[2])
+
+        ustart, unum = parse_sfile(subuid_f, user)
+        gstart, gnum = parse_sfile(subgid_f, group)
+
+        self.user = user
+        self.group = group
+        super(LXCUserIdMap, self).__init__(ustart, unum, gstart, gnum)
