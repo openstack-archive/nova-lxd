59a39e9e2d4df14e4eab459b984b60f4029ff702
Chuck Short <chuck.short@canonical.com>
2015-09-26 21:33:15 -0400

    Remove LXDTestContainerUtils
    Remove tests that doesnt apply any more. Also fix more pep8 errors.
    
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- nclxd/tests/test_container_utils.py
+++ nclxd/tests/test_container_utils.py
@@ -27,21 +27,3 @@ class LXDTestContainerUtils(test.NoDBTestCase):
 
     def setUp(self):
         super(LXDTestContainerUtils, self).setUp()
-        self.ml = stubs.lxd_mock()
-        lxd_patcher = mock.patch('pylxd.api.API',
-                                 mock.Mock(return_value=self.ml))
-        lxd_patcher.start()
-        self.addCleanup(lxd_patcher.stop)
-
-        self.container_utils = container_utils.LXDContainerUtils()
-
-    def test_wait_undefined(self):
-        self.assertRaises(exception.NovaException,
-                          self.container_utils.wait_for_container,
-                          None)
-
-    def test_wait_timedout(self):
-        self.ml.wait_container_operation.return_value = False
-        self.assertRaises(exception.NovaException,
-                          self.container_utils.wait_for_container,
-                          'fake')
