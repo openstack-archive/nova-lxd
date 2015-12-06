4173177e063d90970cc143c87b9c0a1ff03c4ddc
Chuck Short <chuck.short@canonical.com>
2015-12-06 11:46:41 -0500

    Refactor snapstop LXD functions
    Consolidate container_utils and container_client container
    operations for better readbility and make it easier to maintain.
    
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

---
+++ nova_lxd/nova/virt/lxd/session/snapshot.py
@@ -0,0 +1,170 @@
+# Copyright 2015 Canonical Ltd
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+#    the License for the specific language governing permissions and
+#    limitations under the License.
+
+from nova import exception
+from nova import i18n
+from pylxd import exceptions as lxd_exceptions
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_utils import excutils
+
+_ = i18n._
+_LE = i18n._LE
+_LI = i18n._LI
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+class SnapshotMixin(object):
+
+    def container_copy(self, config, instance):
+        """Copy a LXD container
+
+        :param config: LXD container configuration
+        :param instance: nova instance object
+
+        """
+        LOG.debug('container_copy called for instance', instance=instance)
+        try:
+            LOG.info(_LI('Copying instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+
+            client = self.get_session(instance.host)
+            (state, data) = client.contianer_local_copy(config)
+            self.operation_wait(data.get('operation'), instance)
+            LOG.info(_LI('Successfully copied container %s'),
+                     instance.name, instance=instance)
+
+            LOG.info(_LI('Successfully copied instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+        except lxd_exceptions.APIError as ex:
+            msg = _('Failed to communicate with LXD API %(instance)s:'
+                    ' %(reason)s') % {'instance': instance.name,
+                                      'reason': ex}
+            raise exception.NovaException(msg)
+        except Exception as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.error(
+                    _LE('Failed to copy container %(instance)s: %(reason)s'),
+                    {'instance': instance.name,
+                     'reason': ex})
+
+    def container_move(self, old_name, config, instance):
+        """Move a container from one host to another
+
+        :param old_name: Old container name
+        :param config:  Old container config
+        :param instance: nova instance object
+        :return:
+
+        """
+        LOG.debug('container_move called for instance', instnace=instance)
+        try:
+            LOG.info(_LI('Moving instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+
+            # Container move
+            client = self.get_session(instance.host)
+            (state, data) = client.container_local_move(old_name, config)
+            self.operation_wait(data.get('operation'), instance)
+
+            LOG.info(_LI('Successfully moved instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+        except lxd_exceptions.APIError as ex:
+            msg = _('Failed to communicate with LXD API %(instance)s:'
+                    ' %(reason)s') % {'instance': instance.name,
+                                      'reason': ex}
+            raise exception.NovaException(msg)
+        except Exception as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.error(
+                    _LE('Failed to move container %(instance)s: %('
+                        'reason)s'),
+                    {'instance': instance.name,
+                     'reason': ex}, instance=instance)
+
+    def container_snapshot(self, snapshot, instance):
+        """Snapshot a LXD container
+
+        :param snapshot: snapshot config dictionary
+        :param instance: nova instance object
+
+        """
+        LOG.debug('container_snapshot called for instance', instance=instance)
+        try:
+            LOG.info(_LI('Snapshotting instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+
+            # Container snapshot
+            client = self.get_session(instance.host)
+            (state, data) = client.container_snapshot_create(
+                instance.name, snapshot)
+            self.operation_wait(data.get('operation'), instance)
+
+            LOG.info(_LI('Successfully snapshotted instance %(instance)s with'
+                         '%(image)s'), {'instance': instance.name,
+                                        'image': instance.image_ref})
+        except lxd_exceptions.APIError as ex:
+            msg = _('Failed to communicate with LXD API %(instance)s:'
+                    ' %(reason)s') % {'instance': instance.name,
+                                      'reason': ex}
+            raise exception.NovaException(msg)
+        except Exception as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.error(
+                    _LE('Failed to snapshot container %(instance)s: '
+                        '%(reason)s'),
+                    {'instance': instance.name,
+                     'reason': ex}, instance=instance)
+
+    def container_publish(self, image, instance):
+        """Publish a container to the local LXD image store
+
+        :param image: LXD fingerprint
+        :param instance: nova instance object
+        :return: True if published, False otherwise
+
+        """
+        LOG.debug('container_publish called for instance', instance=instance)
+        try:
+            client = self.get_session(instance.host)
+            return client.image_export(image)
+        except lxd_exceptions.APIError as ex:
+            msg = _('Failed to communicate with LXD API %(instance)s:'
+                    ' %(reason)s') % {'instance': instance.name,
+                                      'reason': ex}
+            raise exception.NovaException(msg)
+        except Exception as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.error(
+                    _LE('Failed to create container %(instance)s: %(reason)s'),
+                    {'instance': instance.name,
+                     'reason': ex}, instance=instance)
+
+    def container_export(self, image, instance):
+        try:
+            client = self.get_session(instance.host)
+            return client.image_export(image)
+        except lxd_exceptions.APIError as ex:
+            msg = _('Failed to export image: %s') % ex
+            raise exception.NovaException(msg)
---
+++ nova_lxd/tests/session/test_snapshot.py
@@ -0,0 +1,81 @@
+# Copyright 2015 Canonical Ltd
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#    implied. See the License for the specific language governing
+#    permissions and limitations under the License.
+
+import ddt
+import mock
+
+from nova import exception
+from nova import test
+from pylxd import exceptions as lxd_exceptions
+
+from nova_lxd.nova.virt.lxd.session import session
+from nova_lxd.tests import fake_api
+from nova_lxd.tests import stubs
+
+
+@ddt.ddt
+class SessionSnapshotTest(test.NoDBTestCase):
+
+    def setUp(self):
+        super(SessionSnapshotTest, self).setUp()
+
+        """This is so we can mock out pylxd API calls."""
+        self.ml = stubs.lxd_mock()
+        lxd_patcher = mock.patch('pylxd.api.API',
+                                 mock.Mock(return_value=self.ml))
+        lxd_patcher.start()
+        self.addCleanup(lxd_patcher.stop)
+
+        self.session = session.LXDAPISession()
+
+    @stubs.annotated_data(
+        ('1,', (200, fake_api.fake_operation_info_ok()))
+    )
+    def test_container_snapshot(self, tag, side_effect):
+        snapshot = mock.Mock()
+        instance = stubs._fake_instance()
+        self.ml.container_snapshot_create.return_value = side_effect
+        self.assertEqual(None,
+                         self.session.container_snapshot(snapshot, instance))
+        calls = [
+            mock.call.container_snapshot_create(instance.name, snapshot),
+            mock.call.wait_container_operation(
+                '/1.0/operation/1234', 200, -1)]
+        self.assertEqual(calls, self.ml.method_calls)
+
+    @stubs.annotated_data(
+        ('api_fail', lxd_exceptions.APIError(500, 'Fake'),
+         exception.NovaException)
+    )
+    def test_container_snapshot_fail(self, tag, side_effect, expected):
+        snapshot = mock.Mock()
+        instance = stubs._fake_instance()
+        self.ml.container_snapshot_create.side_effect = side_effect
+        self.assertRaises(expected,
+                          self.session.container_snapshot,
+                          instance.name, snapshot)
+
+    @stubs.annotated_data(
+        (1, (200, fake_api.fake_operation_info_ok()))
+    )
+    def test_container_publish(self, tag, side_effect):
+        image = mock.Mock()
+        instance = stubs._fake_instance()
+        self.ml.image_export.return_value = True
+        self.assertTrue(
+            self.session.container_publish(image, instance))
+        calls = [
+            mock.call.image_export(image)]
+        self.assertEqual(calls, self.ml.method_calls)
