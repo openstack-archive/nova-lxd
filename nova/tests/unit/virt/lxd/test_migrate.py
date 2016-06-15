481a7628fa32102950fc990c743798588a525948
Chuck Short <chuck.short@canonical.com>
2016-06-14 20:39:24 -0400

    Add support for live migration
    Add support for live migrating nova intsances
    from one host to another.
    
    To turn on live migration support, the
    allow_live_migrate flag must be set to true.
    This is disabled by default because the
    feaure in LXD is still considered expermitenal.
    
    This patch does several things:
    
    * Allows the container_init and wait_operation
      and operation_info to be called remotely.
      This is done so that the container
      initializing is done on the correct host.
    * Split out the container_init method and
      container copy methods when a migration
      takes place so that it can be used
      for both types of live migration.
    * Simplify the container configuration for the
      LXD instance when performing a migration.
      So that both live and non-live migration can
      use it.
    * Add unit tests.
    
    Change-Id: Id17ccf0b3eb2971d77c78c1717c36558af82975a
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- nova/tests/unit/virt/lxd/test_migrate.py
+++ nova/tests/unit/virt/lxd/test_migrate.py
@@ -16,14 +16,14 @@
 import mock
 
 import nova.conf
+from nova import exception
 from nova import test
+from nova.tests.unit import fake_instance
+from nova.tests.unit import fake_network
 from nova.virt import fake
 
-from nova.virt.lxd import config
 from nova.virt.lxd import migrate
-from nova.virt.lxd import operations
 from nova.virt.lxd import session
-import stubs
 
 CONF = nova.conf.CONF
 
@@ -35,55 +35,139 @@ class LXDTestContainerMigrate(test.NoDBTestCase):
 
         self.migrate = migrate.LXDContainerMigrate(
             fake.FakeVirtAPI())
+        self.context = 'fake_context'
+        self.migrate.session = mock.MagicMock()
+        self.migrate.config = mock.MagicMock()
+        self.migrate.operations = mock.MagicMock()
 
-    def test_migrate_disk_power_off_resize(self):
+    @mock.patch.object(session.LXDAPISession, 'container_defined')
+    def test_migrate_disk_and_power_off_different_host(
+            self, mock_container_defined):
+        """Verify that conianer stops when Nova starts to
+           migrate an instance to a different host.
+        """
+        mock_instance = fake_instance.fake_instance_obj(self.context)
+        self.migrate.migrate_disk_and_power_off(
+            mock.sentinel.context, mock_instance,
+            mock.sentinel.dest, mock.sentinel.flavor,
+            mock.sentinel.network_info,
+            mock.sentinel.block_device_info,
+            mock.sentinel.timeout, mock.sentinel.retry_interval)
+        mock_container_defined.return_value = True
+        self.migrate.session.container_stop.assert_called_once_with(
+            mock_instance.name, mock_instance
+        )
+
+    @mock.patch.object(session.LXDAPISession, 'container_defined')
+    def test_confirm_migration(self, mock_contaienr_defined):
+        """Verify that the correct migration container calls
+           are made.
+        """
+        mock_instance = fake_instance.fake_instance_obj(self.context)
+        fake_network_info = fake_network.fake_get_instance_nw_info
+        self.migrate.confirm_migration(
+            mock.sentinel.migration, mock_instance, fake_network_info)
+        self.migrate.session.profile_delete.assert_called_once_with(
+            mock_instance
+        )
+        self.migrate.session.container_destroy.assert_called_once_with(
+            mock_instance.name, mock_instance
+        )
+        self.migrate.operations.unplug_vifs.assert_called_once_with(
+            mock_instance, fake_network_info
+        )
+
+
+class LXDTestLiveMigrate(test.NoDBTestCase):
+
+    def setUp(self):
+        super(LXDTestLiveMigrate, self).setUp()
+
+        self.migrate = migrate.LXDContainerMigrate(
+            fake.FakeVirtAPI())
+        self.context = 'fake_context'
+        self.migrate = migrate.LXDContainerMigrate(
+            fake.FakeVirtAPI())
+        self.migrate.session = mock.MagicMock()
+        self.migrate.config = mock.MagicMock()
+        self.migrate.operations = mock.MagicMock()
+
+    def test_copy_container_profile(self):
+        """Verify the correct calls are made
+           when a host needs to copy a contianer profile.
+        """
+        mock_instance = fake_instance.fake_instance_obj(self.context)
+        fake_network_info = fake_network.fake_get_instance_nw_info
+
+        self.migrate._copy_container_profile(
+            mock_instance, fake_network_info)
+        self.migrate.config.create_profile.assert_called_once_with(
+            mock_instance, fake_network_info)
+        self.migrate.session.profile_create.assert_called_once_with(
+            mock.call.create_proile, mock_instance)
+
+    @mock.patch.object(migrate.LXDContainerMigrate, '_copy_container_profile')
+    def test_pre_live_migration(self, mock_container_profile):
+        """Verify that the copy profile methos is called."""
+        self.migrate.pre_live_migration(
+            mock.sentinel.context, mock.sentinel.instance,
+            mock.sentinel.block_device_info,
+            mock.sentinel.network_info,
+            mock.sentinel.disk_info,
+            mock.sentinel.migrate_data)
+
+        mock_container_profile.assert_called_once_with(
+            mock.sentinel.instance, mock.sentinel.network_info)
+
+    @mock.patch.object(migrate.LXDContainerMigrate, '_container_init')
+    def test_live_migration(self, mock_container_init):
+        """Verify that the correct live migration calls
+           are made.
+        """
         self.flags(my_ip='fakeip')
-        instance = stubs._fake_instance()
-        network_info = mock.Mock()
-        flavor = mock.Mock()
-        context = mock.Mock()
-        dest = 'fakeip'
+        mock_post_method = mock.MagicMock()
+        self.migrate.live_migration(
+            mock.sentinel.context, mock.sentinel.instance,
+            mock.sentinel.dest, mock_post_method,
+            mock.sentinel.recover_method, mock.sentinel.block_migration,
+            mock.sentinel.migrate_data)
+        mock_container_init.assert_called_once_with(CONF.my_ip,
+                                                    mock.sentinel.instance)
+        mock_post_method.assert_called_once_with(
+            mock.sentinel.context, mock.sentinel.instance, mock.sentinel.dest,
+            mock.sentinel.block_migration, host=mock.sentinel.dest)
 
-        with test.nested(
-            mock.patch.object(session.LXDAPISession, 'container_defined'),
-            mock.patch.object(config.LXDContainerConfig, 'create_profile'),
-            mock.patch.object(session.LXDAPISession, 'profile_update')
-        ) as (
-            mock_container_defined,
-            mock_create_profile,
-            mock_profile_update
-        ):
-            self.assertEqual('',
-                             self.migrate.migrate_disk_and_power_off(
-                                 context, instance, dest, flavor,
-                                 network_info))
-            mock_container_defined.assert_called_once_with(instance.name,
-                                                           instance)
-            mock_create_profile.assert_called_once_with(instance,
-                                                        network_info)
+    def test_post_live_migration(self):
+        """Verify that the correct post_live_migration calls
+           are made.
+        """
+        mock_instance = fake_instance.fake_instance_obj(self.context)
+        self.migrate.post_live_migration(
+            mock.sentinel.context, mock_instance,
+            mock.sentinel.block_device_info, mock.sentinel.migrate_data)
+        self.migrate.session.container_destroy.assert_called_once_with(
+            mock_instance.name, mock_instance)
 
-    def test_confirm_migration(self):
-        migration = mock.Mock()
-        instance = stubs._fake_instance()
-        network_info = mock.Mock()
+    def test_live_migration_not_allowed(self):
+        """Verify an exception is raised when live migration is not allowed."""
+        self.flags(allow_live_migrate=False,
+                   group='lxd')
+        self.assertRaises(exception.MigrationPreCheckError,
+                self.migrate.check_can_live_migrate_source,
+                    mock.sentinel.context, mock.sentinel.instance,
+                    mock.sentinel.dest_check_data,
+                    mock.sentinel.block_device_info)
+    
+    def test_live_migration_allowed(self):
+        """Verify live-migration is allowed when the allow_lvie_migrate
+           flag is True.
+        """
+        self.flags(allow_live_migrate=True,
+                   group='lxd')
+        self.assertEqual(mock.sentinel.dest_check_data,
+                         self.migrate.check_can_live_migrate_source(
+                            mock.sentinel.context, mock.sentinel.instance,
+                            mock.sentinel.dest_check_data,
+                            mock.sentinel.block_device_info))
 
-        with test.nested(
-            mock.patch.object(session.LXDAPISession, 'container_defined'),
-            mock.patch.object(session.LXDAPISession, 'profile_delete'),
-            mock.patch.object(session.LXDAPISession, 'container_destroy'),
-            mock.patch.object(operations.LXDContainerOperations,
-                              'unplug_vifs'),
-        ) as (
-                mock_container_defined,
-                mock_profile_delete,
-                mock_container_destroy,
-                mock_unplug_vifs):
-            self.assertEqual(None,
-                             self.migrate.confirm_migration(migration,
-                                                            instance,
-                                                            network_info))
-            mock_container_defined.assert_called_once_with(instance.name,
-                                                           instance)
-            mock_profile_delete.assert_called_once_with(instance)
-            mock_unplug_vifs.assert_called_once_with(instance,
-                                                     network_info)
+
