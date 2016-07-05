8b8a662a286e98a4f2ed2c2013e822e28ffa67ed
Chuck Short <chuck.short@canonical.com>
2016-07-05 10:55:27 -0400

    Add support for ephemeral storage
    Add previously missing ephemeral storage support for nova-lxd.
    This patch provides the infrastructure  to translate
    the configuration of the instance to something
    that LXD can understand and act upon.
    
    The way that the ephemeral storage works is that block_device_info
    dict and instance object. If an empeheral storage device
    is needed then the storage deivce is created on the compute host
    and bind mounted into the container when the container is started.
    This is due to the contanier is running unprivileged and restricted
    mount(8)
    
    To come is restricting the ephemeral storage based on the LXD storage
    backend on the compute host.
    
    Update exist unit tests and adds unit new tests.
    
    Change-Id: I38d48708b3c6fb450258e03a6106f47be3aeb998
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- nova/tests/unit/virt/lxd/stubs.py
+++ nova/tests/unit/virt/lxd/stubs.py
@@ -38,7 +38,8 @@ class MockConf(mock.Mock):
         lxd_default = {
             'root_dir': '/fake/lxd/root',
             'timeout': 20,
-            'retry_interval': 2
+            'retry_interval': 2,
+            'storage_driver': 'fs'
         }
         lxd_default.update(lxd_kwargs)
         self.lxd = mock.Mock(lxd_args, **lxd_default)
