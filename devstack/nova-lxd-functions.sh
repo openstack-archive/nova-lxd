de8d223fc394361790c053dff71f88458122c8f8
Pushkar Umaranikar <pushkar.umaranikar@intel.com>
2016-07-07 20:42:22 +0000

    lxd installation with devstack on ubuntu 14.04
    lxd installation with devstack on ubuntu 14.04 requires ubuntu-lxc
    PPA. We can also install it using trusty-backports. otherwise
    devstack installation throws error saying unable to locate package
    lxd.
    
    This change is about adding ubuntu-lxc PPA before we start
    installing lxd. It also includes uninstalling default
    reuqests and urllib3 python packages in Ubuntu 14.04 which
    causes version conflicts in devstack installtion.
    
    Change-Id: I254d48db09b6bce6befdd94cd4ccd7b95bdf029b

--- devstack/nova-lxd-functions.sh
+++ devstack/nova-lxd-functions.sh
@@ -12,6 +12,9 @@ function install_pylxd {
     # Install the latest pylxd from git
     echo_summary "Installing pylxd"
     git_clone_by_name pylxd
+    if [ "$DISTRO" == "trusty" ]; then
+        uninstall_package python-requests python-urllib3
+    fi
     setup_dev_lib "pylxd"
     echo_summary "Pylxd install complete"
 }
@@ -24,11 +27,14 @@ function cleanup_pylxd {
 function install_lxd {
     echo_summary "Checing LXD installation"
     if is_ubuntu; then
+        if [ "$DISTRO" == "trusty" ]; then
+            sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
+        fi
         if ! ( is_package_installed lxd ); then
             install_package lxd
         fi
 
-        add_user_to_group $STACK_USER $LXD_GROUP 
+        add_user_to_group $STACK_USER $LXD_GROUP
     fi
     echo_summary "Installing LXD"
 }
