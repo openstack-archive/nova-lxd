28f265a1aadc03ad4bb7878044e7b674e5c5f113
Chuck Short <chuck.short@canonical.com>
2016-09-08 14:18:25 -0400

    Check for container exists before deletion
    When a user creates an instance, the user is able to destroy
    the instance before LXD has a chance to start the container.
    This results in a bad state between LXD and nova-lxd and
    pylxd raising an exception that the container was not found.
    
    Before destroying the conainer, check the status code
    of LXD to see if the container exists. If the container
    does not exist just cleanup the profile if it exists
    and the assoicated vifs if had already been created.
    
    This was discovered by running tempest unit tests.
    
    Change-Id: I7c244e8bca9bab69ead5d45c966674e9e7107f20
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- contrib/tempest/run_tempest_lxd.sh
+++ contrib/tempest/run_tempest_lxd.sh
@@ -18,9 +18,6 @@ ignored_tests="|^tempest.api.compute.images"
 
 # Regressions
 ignored_tests="$ignored_tests|.*AttachInterfacesTestJSON.test_create_list_show_delete_interfaces"
-ignored_tests="$ignored_tests|.*DeleteServersAdminTestJSON.test_admin_delete_servers_of_others"
-ignored_tests="$ignored_tests|.*DeleteServersAdminTestJSON.test_delete_server_while_in_error_state"
-ignored_tests="$ignored_tests|.*DeleteServersTestJSON.test_delete_server_while_in_building_state"
 
 # backups are not supported
 ignored_tests="$ignored_tests|.*ServerActionsTestJSON.test_create_backup"
