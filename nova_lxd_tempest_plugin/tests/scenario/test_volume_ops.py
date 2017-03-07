bc64a4389dad127e836cf8f36ea57895f1cb9f56
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-03-07 15:01:13 -0800

    Switch to use stable data_utils
    Tempest provides stable library interfaces under tempest.lib.
    This patch switches to use it for data_utils.
    
    Depends-On: Ib3e29a2b03429e69151717908bc43c590bb8c52e
    Change-Id: I0a08bdfd304d988ca48fb07d901d3ecfd5968599
    (cherry picked from commit 9a92d8bcf545ee3d5c483c04a5d7b68f22147990)

--- nova_lxd_tempest_plugin/tests/scenario/test_volume_ops.py
+++ nova_lxd_tempest_plugin/tests/scenario/test_volume_ops.py
@@ -19,10 +19,10 @@ import time
 from oslo_log import log as logging
 import testtools
 
-from tempest.common.utils import data_utils
 from tempest.common import waiters
 from tempest import config
 from tempest import exceptions
+from tempest.lib.common.utils import data_utils
 from tempest.lib.common.utils import test_utils
 from tempest.lib import decorators
 from tempest.lib import exceptions as lib_exc
