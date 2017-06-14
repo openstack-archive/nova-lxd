056ccef3ece8f81b33817c891505aaa23a750c10
James Page <james.page@ubuntu.com>
2017-06-14 15:57:47 +0100

    vif: redux interface wiring approach
    The nova-lxd driver has to take a slightly different approach
    to virtual interface wiring due to a lack of an equivalent to
    'launch and pause' in LXD.
    
    For some interface types, the last mile tap device needs to
    be present for vif plugging to complete successfully which
    occurs prior to the instance being launched; This change
    refactors the vif module to create veth pairs directly
    in nova-lxd, rather than delegating this to LXD as part of
    a bridged network interface type.  This allows vif plugging
    to complete prior to the instance being created in LXD.
    
    The side effect of this change is that all currently supported
    interface types are now configured as 'physical' interfaces
    in LXD profiles for instances - wiring to bridges is handled
    directly by the nova-lxd driver instead.
    
    This change has been validated with:
    
       ovs driver + iptables hybrid firewall driver
       ovs driver + openvswitch native firewall driver
       linuxbridge driver + iptables hybrid firewall driver
    
    The VIF wiring approach is described in detail in the VIF
    wiring documentation included in this change.
    
    Closes-Bug: 1681758
    Change-Id: Ic268e989d1ee19f696298fb1e0db729a00352a12

--- nova/virt/lxd/driver.py
+++ nova/virt/lxd/driver.py
@@ -662,46 +662,39 @@ class LXDDriver(driver.ComputeDriver):
 
         profile = self.client.profiles.get(instance.name)
 
-        network_config = lxd_vif.get_config(vif)
-
-        # XXX(jamespage): Refactor into vif module so code is shared
-        #                 across hotplug and instance creation.
-        if 'bridge' in network_config:
-            net_device = network_config['bridge']
-            config_update = {
-                net_device: {
-                    'nictype': 'bridged',
-                    'hwaddr': vif['address'],
-                    'parent': network_config['bridge'],
-                    'type': 'nic',
-                }
+        net_device = lxd_vif.get_vif_devname(vif)
+        config_update = {
+            net_device: {
+                'nictype': 'physical',
+                'hwaddr': vif['address'],
+                'parent': lxd_vif.get_vif_internal_devname(vif),
+                'type': 'nic',
             }
-        else:
-            net_device = lxd_vif.get_vif_devname(vif)
-            config_update = {
-                net_device: {
-                    'nictype': 'physical',
-                    'hwaddr': vif['address'],
-                    'parent': lxd_vif.get_vif_internal_devname(vif),
-                    'type': 'nic',
-                }
-            }
+        }
 
         profile.devices.update(config_update)
         profile.save(wait=True)
 
     def detach_interface(self, context, instance, vif):
         profile = self.client.profiles.get(instance.name)
-        to_remove = None
-        # XXX(jamespage): refactor to use actual key
-        #                 which switch to consistent
-        #                 device naming in the profile.
-        for key, val in profile.devices.items():
-            if val.get('hwaddr') == vif['address']:
-                to_remove = key
-                break
-        del profile.devices[to_remove]
-        profile.save(wait=True)
+        devname = lxd_vif.get_vif_devname(vif)
+
+        # NOTE(jamespage): Attempt to remove device using
+        #                  new style tap naming
+        if devname in profile.devices:
+            del profile.devices[devname]
+            profile.save(wait=True)
+        else:
+            # NOTE(jamespage): For upgrades, scan devices
+            #                  and attempt to identify
+            #                  using mac address as the
+            #                  device will *not* have a
+            #                  consistent name
+            for key, val in profile.devices.items():
+                if val.get('hwaddr') == vif['address']:
+                    del profile.devices[key]
+                    profile.save(wait=True)
+                    break
 
         self.vif_driver.unplug(instance, vif)
 
