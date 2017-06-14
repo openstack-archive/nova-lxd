ce784c669b197ffb286ca845c456f34e583840df
James Page <james.page@ubuntu.com>
2017-06-14 15:57:47 +0100

    vif: redux interface wiring approach
    The nova-lxd driver has to take a slightly different approach
    to virtual interface wiring due to a lack of an equivalent to
    'launch and pause' in LXD.
    
    For some interface types, the last mile tap device needs to
    be present for vif plugging to complete successfully which
    occurs prior to the instance being launched; This change
    refactors the vif module to create veth pairs directly
    in nova-lxd, rather than delegating this to LXD as part of
    a bridged network interface type.  This allows vif plugging
    to complete prior to the instance being created in LXD.
    
    The side effect of this change is that all currently supported
    interface types are now configured as 'physical' interfaces
    in LXD profiles for instances - wiring to bridges is handled
    directly by the nova-lxd driver instead.
    
    This change has been validated with:
    
       ovs driver + iptables hybrid firewall driver
       ovs driver + openvswitch native firewall driver
       linuxbridge driver + iptables hybrid firewall driver
    
    The VIF wiring approach is described in detail in the VIF
    wiring documentation included in this change.
    
    Closes-Bug: 1681758
    Change-Id: Ic268e989d1ee19f696298fb1e0db729a00352a12

--- nova/tests/unit/virt/lxd/test_vif.py
+++ nova/tests/unit/virt/lxd/test_vif.py
@@ -27,17 +27,30 @@ SUBNET = network_model.Subnet(
     cidr='101.168.1.0/24', dns=[DNS_BRIDGE], gateway=GATEWAY,
     routes=None, dhcp_server='191.168.1.1')
 NETWORK = network_model.Network(
-    id='network-id-xxx-yyy-zzz', bridge='br0', label=None,
+    id='ab7b876b-2c1c-4bb2-afa1-f9f4b6a28053', bridge='br0', label=None,
     subnets=[SUBNET], bridge_interface=None, vlan=99, mtu=1000)
-VIF = network_model.VIF(
-    id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+OVS_VIF = network_model.VIF(
+    id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8', address='ca:fe:de:ad:be:ef',
     network=NETWORK, type=network_model.VIF_TYPE_OVS,
-    devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+    devname='tapda5cc4bf-f1',
+    ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638',
+    details={network_model.VIF_DETAILS_OVS_HYBRID_PLUG: False})
+OVS_HYBRID_VIF = network_model.VIF(
+    id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8', address='ca:fe:de:ad:be:ef',
+    network=NETWORK, type=network_model.VIF_TYPE_OVS,
+    devname='tapda5cc4bf-f1',
+    ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638',
+    details={network_model.VIF_DETAILS_OVS_HYBRID_PLUG: True})
 TAP_VIF = network_model.VIF(
-    id='0123456789abcdef', address='ca:fe:de:ad:be:ee',
+    id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8', address='ca:fe:de:ad:be:ee',
     network=NETWORK, type=network_model.VIF_TYPE_TAP,
-    devname='tap-014-345-678',
+    devname='tapda5cc4bf-f1',
     details={'mac_address': 'aa:bb:cc:dd:ee:ff'})
+LB_VIF = network_model.VIF(
+    id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8', address='ca:fe:de:ad:be:ed',
+    network=NETWORK, type=network_model.VIF_TYPE_BRIDGE,
+    devname='tapda5cc4bf-f1')
+
 INSTANCE = fake_instance.fake_instance_obj(
     context.get_admin_context(), name='test')
 
@@ -47,7 +60,7 @@ class GetVifDevnameTest(test.NoDBTestCase):
 
     def test_get_vif_devname_devname_exists(self):
         an_vif = {
-            'id': '0123456789abcdef',
+            'id': 'da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
             'devname': 'oth1',
         }
 
@@ -57,12 +70,12 @@ class GetVifDevnameTest(test.NoDBTestCase):
 
     def test_get_vif_devname_devname_nonexistent(self):
         an_vif = {
-            'id': '0123456789abcdef',
+            'id': 'da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
         }
 
         devname = vif.get_vif_devname(an_vif)
 
-        self.assertEqual('nic0123456789a', devname)
+        self.assertEqual('nicda5cc4bf-f1', devname)
 
 
 class GetConfigTest(test.NoDBTestCase):
@@ -70,7 +83,7 @@ class GetConfigTest(test.NoDBTestCase):
 
     def setUp(self):
         super(GetConfigTest, self).setUp()
-        self.CONF_patcher = mock.patch('nova.virt.lxd.vif.conf.CONF')
+        self.CONF_patcher = mock.patch('nova.virt.lxd.vif.CONF')
         self.CONF = self.CONF_patcher.start()
         self.CONF.firewall_driver = 'nova.virt.firewall.NoopFirewallDriver'
 
@@ -81,9 +94,11 @@ class GetConfigTest(test.NoDBTestCase):
     def test_get_config_bad_vif_type(self):
         """Unsupported vif types raise an exception."""
         an_vif = network_model.VIF(
-            id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+            id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            address='ca:fe:de:ad:be:ef',
             network=NETWORK, type='invalid',
-            devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+            devname='tapda5cc4bf-f1',
+            ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638')
 
         self.assertRaises(
             exception.NovaException, vif.get_config, an_vif)
@@ -91,9 +106,11 @@ class GetConfigTest(test.NoDBTestCase):
     def test_get_config_bridge(self):
         expected = {'bridge': 'br0', 'mac_address': 'ca:fe:de:ad:be:ef'}
         an_vif = network_model.VIF(
-            id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+            id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            address='ca:fe:de:ad:be:ef',
             network=NETWORK, type='bridge',
-            devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+            devname='tapda5cc4bf-f1',
+            ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638')
 
         config = vif.get_config(an_vif)
 
@@ -103,9 +120,11 @@ class GetConfigTest(test.NoDBTestCase):
         expected = {
             'bridge': 'br0', 'mac_address': 'ca:fe:de:ad:be:ef'}
         an_vif = network_model.VIF(
-            id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+            id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            address='ca:fe:de:ad:be:ef',
             network=NETWORK, type='ovs',
-            devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+            devname='tapda5cc4bf-f1',
+            ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638')
 
         config = vif.get_config(an_vif)
 
@@ -115,11 +134,13 @@ class GetConfigTest(test.NoDBTestCase):
         self.CONF.firewall_driver = 'AnFirewallDriver'
 
         expected = {
-            'bridge': 'qbr0123456789a', 'mac_address': 'ca:fe:de:ad:be:ef'}
+            'bridge': 'qbrda5cc4bf-f1', 'mac_address': 'ca:fe:de:ad:be:ef'}
         an_vif = network_model.VIF(
-            id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+            id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            address='ca:fe:de:ad:be:ef',
             network=NETWORK, type='ovs',
-            devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+            devname='tapda5cc4bf-f1',
+            ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638')
 
         config = vif.get_config(an_vif)
 
@@ -128,9 +149,11 @@ class GetConfigTest(test.NoDBTestCase):
     def test_get_config_tap(self):
         expected = {'mac_address': 'ca:fe:de:ad:be:ef'}
         an_vif = network_model.VIF(
-            id='0123456789abcdef', address='ca:fe:de:ad:be:ef',
+            id='da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            address='ca:fe:de:ad:be:ef',
             network=NETWORK, type='tap',
-            devname='tap-012-345-678', ovs_interfaceid='9abc-def-000')
+            devname='tapda5cc4bf-f1',
+            ovs_interfaceid='7b6812a6-b044-4596-b3c5-43a8ec431638')
 
         config = vif.get_config(an_vif)
 
@@ -144,36 +167,165 @@ class LXDGenericVifDriverTest(test.NoDBTestCase):
         super(LXDGenericVifDriverTest, self).setUp()
         self.vif_driver = vif.LXDGenericVifDriver()
 
-    @mock.patch('nova.virt.lxd.vif.os_vif')
-    def test_plug_ovs(self, os_vif):
-        self.vif_driver.plug(INSTANCE, VIF)
-
-        self.assertEqual(
-            'tap-012-345-678', os_vif.plug.call_args[0][0].vif_name)
-        self.assertEqual(
-            'instance-00000001', os_vif.plug.call_args[0][1].name)
-
-    @mock.patch('nova.virt.lxd.vif.os_vif')
-    def test_unplug_ovs(self, os_vif):
-        self.vif_driver.unplug(INSTANCE, VIF)
-
-        self.assertEqual(
-            'tap-012-345-678', os_vif.unplug.call_args[0][0].vif_name)
-        self.assertEqual(
-            'instance-00000001', os_vif.unplug.call_args[0][1].name)
-
-    @mock.patch('nova.virt.lxd.vif.os_vif')
-    def test_plug_tap(self, os_vif):
-        with mock.patch.object(vif, '_create_veth_pair') as create_veth_pair:
-            self.vif_driver.plug(INSTANCE, TAP_VIF)
-            os_vif.plug.assert_not_called()
-            create_veth_pair.assert_called_with('tap-014-345-678',
-                                                'tin-014-345-678',
-                                                1000)
-
+    @mock.patch.object(vif, '_post_plug_wiring')
     @mock.patch('nova.virt.lxd.vif.linux_net')
     @mock.patch('nova.virt.lxd.vif.os_vif')
-    def test_unplug_tap(self, os_vif, linux_net):
+    def test_plug_ovs(self, os_vif, linux_net, _post_plug_wiring):
+        self.vif_driver.plug(INSTANCE, OVS_VIF)
+
+        self.assertEqual(
+            'tapda5cc4bf-f1', os_vif.plug.call_args[0][0].vif_name)
+        self.assertEqual(
+            'instance-00000001', os_vif.plug.call_args[0][1].name)
+        _post_plug_wiring.assert_called_with(INSTANCE, OVS_VIF)
+
+    @mock.patch.object(vif, '_post_unplug_wiring')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    @mock.patch('nova.virt.lxd.vif.os_vif')
+    def test_unplug_ovs(self, os_vif, linux_net, _post_unplug_wiring):
+        self.vif_driver.unplug(INSTANCE, OVS_VIF)
+
+        self.assertEqual(
+            'tapda5cc4bf-f1', os_vif.unplug.call_args[0][0].vif_name)
+        self.assertEqual(
+            'instance-00000001', os_vif.unplug.call_args[0][1].name)
+        _post_unplug_wiring.assert_called_with(INSTANCE, OVS_VIF)
+
+    @mock.patch.object(vif, '_post_plug_wiring')
+    @mock.patch.object(vif, '_create_veth_pair')
+    @mock.patch('nova.virt.lxd.vif.os_vif')
+    def test_plug_tap(self, os_vif, _create_veth_pair, _post_plug_wiring):
+        self.vif_driver.plug(INSTANCE, TAP_VIF)
+        os_vif.plug.assert_not_called()
+        _create_veth_pair.assert_called_with('tapda5cc4bf-f1',
+                                             'tinda5cc4bf-f1',
+                                             1000)
+        _post_plug_wiring.assert_called_with(INSTANCE, TAP_VIF)
+
+    @mock.patch.object(vif, '_post_unplug_wiring')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    @mock.patch('nova.virt.lxd.vif.os_vif')
+    def test_unplug_tap(self, os_vif, linux_net, _post_unplug_wiring):
         self.vif_driver.unplug(INSTANCE, TAP_VIF)
         os_vif.plug.assert_not_called()
-        linux_net.delete_net_dev.assert_called_with('tap-014-345-678')
+        linux_net.delete_net_dev.assert_called_with('tapda5cc4bf-f1')
+        _post_unplug_wiring.assert_called_with(INSTANCE, TAP_VIF)
+
+
+class PostPlugTest(test.NoDBTestCase):
+    """Tests for post plug operations"""
+
+    def setUp(self):
+        super(PostPlugTest, self).setUp()
+
+    @mock.patch('nova.virt.lxd.vif._create_veth_pair')
+    @mock.patch('nova.virt.lxd.vif._add_bridge_port')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_plug_ovs_hybrid(self,
+                                  linux_net,
+                                  add_bridge_port,
+                                  create_veth_pair):
+        linux_net.device_exists.return_value = False
+
+        vif._post_plug_wiring(INSTANCE, OVS_HYBRID_VIF)
+
+        linux_net.device_exists.assert_called_with('tapda5cc4bf-f1')
+        create_veth_pair.assert_called_with('tapda5cc4bf-f1',
+                                            'tinda5cc4bf-f1',
+                                            1000)
+        add_bridge_port.assert_called_with('qbrda5cc4bf-f1',
+                                           'tapda5cc4bf-f1')
+
+    @mock.patch('nova.virt.lxd.vif._create_veth_pair')
+    @mock.patch('nova.virt.lxd.vif._add_bridge_port')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_plug_ovs(self,
+                           linux_net,
+                           add_bridge_port,
+                           create_veth_pair):
+
+        linux_net.device_exists.return_value = False
+
+        vif._post_plug_wiring(INSTANCE, OVS_VIF)
+
+        linux_net.device_exists.assert_called_with('tapda5cc4bf-f1')
+        create_veth_pair.assert_called_with('tapda5cc4bf-f1',
+                                            'tinda5cc4bf-f1',
+                                            1000)
+        add_bridge_port.assert_not_called()
+        linux_net.create_ovs_vif_port.assert_called_with(
+            'br0',
+            'tapda5cc4bf-f1',
+            'da5cc4bf-f16c-4807-a0b6-911c7c67c3f8',
+            'ca:fe:de:ad:be:ef',
+            INSTANCE.uuid,
+            1000
+        )
+
+    @mock.patch('nova.virt.lxd.vif._create_veth_pair')
+    @mock.patch('nova.virt.lxd.vif._add_bridge_port')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_plug_bridge(self,
+                              linux_net,
+                              add_bridge_port,
+                              create_veth_pair):
+        linux_net.device_exists.return_value = False
+
+        vif._post_plug_wiring(INSTANCE, LB_VIF)
+
+        linux_net.device_exists.assert_called_with('tapda5cc4bf-f1')
+        create_veth_pair.assert_called_with('tapda5cc4bf-f1',
+                                            'tinda5cc4bf-f1',
+                                            1000)
+        add_bridge_port.assert_called_with('br0',
+                                           'tapda5cc4bf-f1')
+
+    @mock.patch('nova.virt.lxd.vif._create_veth_pair')
+    @mock.patch('nova.virt.lxd.vif._add_bridge_port')
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_plug_tap(self,
+                           linux_net,
+                           add_bridge_port,
+                           create_veth_pair):
+        linux_net.device_exists.return_value = False
+
+        vif._post_plug_wiring(INSTANCE, TAP_VIF)
+
+        linux_net.device_exists.assert_not_called()
+
+
+class PostUnplugTest(test.NoDBTestCase):
+    """Tests for post unplug operations"""
+
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_unplug_ovs_hybrid(self, linux_net):
+        vif._post_unplug_wiring(INSTANCE, OVS_HYBRID_VIF)
+        linux_net.delete_net_dev.assert_called_with('tapda5cc4bf-f1')
+
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_unplug_ovs(self, linux_net):
+        vif._post_unplug_wiring(INSTANCE, OVS_VIF)
+        linux_net.delete_ovs_vif_port.assert_called_with('br0',
+                                                         'tapda5cc4bf-f1',
+                                                         True)
+
+    @mock.patch('nova.virt.lxd.vif.linux_net')
+    def test_post_unplug_bridge(self, linux_net):
+        vif._post_unplug_wiring(INSTANCE, LB_VIF)
+        linux_net.delete_net_dev.assert_called_with('tapda5cc4bf-f1')
+
+
+class MiscHelpersTest(test.NoDBTestCase):
+    """Misc tests for vif module"""
+
+    def test_is_ovs_vif_port(self):
+        self.assertTrue(vif._is_ovs_vif_port(OVS_VIF))
+        self.assertFalse(vif._is_ovs_vif_port(OVS_HYBRID_VIF))
+        self.assertFalse(vif._is_ovs_vif_port(TAP_VIF))
+
+    @mock.patch.object(vif, 'utils')
+    def test_add_bridge_port(self, utils):
+        vif._add_bridge_port('br-int', 'tapXYZ')
+        utils.execute.assert_called_with('brctl', 'addif',
+                                         'br-int', 'tapXYZ',
+                                         run_as_root=True)
