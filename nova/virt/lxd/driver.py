1b8953e04355b27c7e1bd5b4fdbac7fbfaebc4d4
Jimmy McCrory <jimmy.mccrory@gmail.com>
2017-10-29 21:04:12 -0700

    Update driver spawn function with new argument
    A recent change in nova[1] added an allocations argument to the
    compute driver spawn function. Include it in nova-lxd's implementation
    as well.
    
    [1] https://review.openstack.org/#/c/511879/
    
    Change-Id: Ie99ef6c7249b0b43cf21de6aaa883d04cdfafba2

--- nova/virt/lxd/driver.py
+++ nova/virt/lxd/driver.py
@@ -20,6 +20,7 @@ import io
 import json
 import os
 import platform
+import pprint
 import pwd
 import shutil
 import socket
@@ -474,7 +475,8 @@ class LXDDriver(driver.ComputeDriver):
         return [c.name for c in self.client.containers.all()]
 
     def spawn(self, context, instance, image_meta, injected_files,
-              admin_password, network_info=None, block_device_info=None):
+              admin_password, allocations, network_info=None,
+              block_device_info=None):
         """Create a new lxd container as a nova instance.
 
         Creating a new container requires a number of steps. First, the
@@ -643,6 +645,7 @@ class LXDDriver(driver.ComputeDriver):
             shutil.rmtree(container_dir)
 
         try:
+            LOG.info(pprint.pprint(self.client.profiles.get(instance.name)))
             self.client.profiles.get(instance.name).delete()
         except lxd_exceptions.LXDAPIException as e:
             if e.response.status_code == 404:
