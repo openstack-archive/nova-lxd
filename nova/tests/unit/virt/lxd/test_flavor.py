aea0499ca62da8a4d3ae4d5678a94e74ae222d53
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-11-12 17:26:14 +0000

    Fix sizing and quotas nova to lxd on unsupported backends
    The dir and lvm backends don't support sizing and quotas.  This was made
    more complicated with LXD3 as "storage" became the default and the dir
    backend, if used, has to be set up as a storage pool.  LXD3 is the
    default on bionic.  To resolve an openstack-ansible issue and lay the
    ground work for nova-lxd and storage pools and the lxd charm, this patch
    makes resolves sending sizes and quotas for a profile root device on lvm
    and dir backends.
    
    Change-Id: Idba4f5f3c6015616160d85f25a658cb7e5c43652
    Closes-Bug: #1698761

--- nova/tests/unit/virt/lxd/test_flavor.py
+++ nova/tests/unit/virt/lxd/test_flavor.py
@@ -132,7 +132,7 @@ class ToProfileTest(test.NoDBTestCase):
                 'path': '/',
                 'type': 'disk',
                 'pool': 'test_pool',
-                'size': '0GB'
+                #'size': '0GB'
             },
         }
         flavor.to_profile(self.client, instance, network_info, block_info)
